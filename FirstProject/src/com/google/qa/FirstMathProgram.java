37a2e744109e4e80736367e133e9c6cf6ce55bb5
Reaz Patwary <infixtechnology@gmail.com>
2014-08-09 12:35:06 -0400

    first commit

---
+++ FirstProject/src/com/google/qa/FirstMathProgram.java
@@ -0,0 +1,13 @@
+package com.google.qa;
+
+public class FirstMathProgram {
+	public static void main(String[] args) {
+		int firstNumber = 5;
+		int secondNumber = 10;
+		int total;
+		total = firstNumber + secondNumber;
+		System.out.println(total);
+
+	}
+
+}
